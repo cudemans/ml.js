32c8b4c6e281c5656138950110cb9a249072b1ff
Chris Udemans <chris.udemans@gmail.com>
2023-01-30 01:50:55 +0200

    build: initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const math = require("mathjs");
+
+class LinearRegression {
+  constructor(learningRate = 0.001, numIters = 1000) {
+    this.learningRate = learningRate;
+    this.numIters = numIters;
+    this.weights = null;
+    this.bias = null;
+    this.numSamples = null;
+    this.numFeatures = null;
+  }
+
+  fit(X, y) {
+    this.numSamples, (this.numFeatures = X.length), X[0].length;
+    this.weights = new Array(this.numFeatures).fill(0);
+    this.bias = 0;
+
+    for (let i = 0; i < this.numIters; i++) {
+      let yPred = math.dot(X, this.weights) + this.bias;
+
+      let yPredArray = Array.isArray(yPred)
+        ? math.dot(X, this.weights) + this.bias
+        : [math.dot(X, this.weights) + this.bias];
+
+      let dw =
+        (1 / this.numSamples) *
+        math.dot(
+          math.transpose(X),
+          yPredArray.map((d, i) => d - y[i])
+        );
+
+      let db =
+        (1 / this.numSamples) *
+        yPred.map((d, i) => d - y[i]).reduce((a, b) => a + b, 0);
+
+      this.weights -= this.learningRate * dw;
+      this.bias -= this.learningRate * db;
+    }
+  }
+
+  predict(X_test) {
+    return math.dot(X_test, this.weights) + this.bias;
+  }
+
+  score(y_pred, y_test) {
+    const mapped = y_pred.map((d, i) => d == y_test[i]);
+    return mapped.reduce((a, b) => a + b, 0) / mapped.length;
+  }
+}
+
+module.exports = { LinearRegression };
