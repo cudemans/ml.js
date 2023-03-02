7308936565faf144085d355d226075a70a2763b7
Chris Udemans <chris.udemans@gmail.com>
2023-03-02 18:26:20 +0200

    build: Update naive bayes predict method (wip)

--- src/naive_bayes/NaiveBayes.js
+++ src/naive_bayes/NaiveBayes.js
@@ -1,10 +1,12 @@
 import mean from "../utils/mean.js";
 import variance from "../utils/variance.js";
+import createMatrix from "../utils/createMatrix.js";
 
 export default class NaiveBayes {
   constructor() {
     this.nSamples = null;
     this.nFeatures = null;
+    this.classes = null;
     this.nClasses = null;
     this.eps = 1e-6;
     this.classesMean = {};
@@ -15,9 +17,10 @@ export default class NaiveBayes {
   fit(X, y) {
     this.nSamples = X.length;
     this.nFeatures = X[0].length;
-    this.nClasses = [...new Set(y)];
+    this.classes = [...new Set(y)];
+    this.nClasses = this.classes.length;
 
-    this.nClasses.forEach((i) => {
+    this.classes.forEach((i) => {
       const mask = y
         .map((d, j) => {
           if (d == i) {
@@ -29,22 +32,39 @@ export default class NaiveBayes {
       const X_c = mask.map((d) => X[d]);
 
       this.classesMean[i] = this.nFeatures < 2 ? mean(X_c, 0) : mean(X_c, 1);
-      console.log(this.classesMean);
       this.classesVar[i] =
         this.nFeatures < 2 ? variance(X_c, 0) : variance(X_c, 1);
       this.classesPrior[i] = X_c.length / this.nSamples;
     });
+    console.log(this.classesMean);
   }
   predict(X) {
-    return;
+    const probs = createMatrix(this.nSamples, this.nClasses, 0);
+
+    this.classes.forEach((d) => {
+      const prior = this.classesPrior[d];
+      const probsC = this.pdf(X, this.classesMean[d], this.classesVar[d]);
+      //   console.log(probs);
+    });
   }
 
   pdf(X, mean, sigma) {
+    // console.log(sigma);
     const c =
       (-this.nFeatures / 2) * Math.log(2 * Math.PI) -
       0.5 * Math.log(sigma + this.eps);
-    const probs = 0.5 * (Math.pow(x - mean, 2) / sigma + self.eps + 1);
+    // const probs = 0.5 * (Math.pow(x - mean, 2) / sigma + this.eps + 1);
 
-    return c - probs;
+    // console.log(
+    //   (-this.nFeatures / 2) * Math.log(2 * Math.PI) - 0.5 * Math.log(this.eps)
+    // );
+
+    const probs = X.map((d) => {
+      return (Math.pow(d - mean, 2) / sigma + this.eps + 1) * 0.5;
+    });
+
+    return probs.map((d) => {
+      return d - c;
+    });
   }
 }
