8e2c8d272036e7a4093010372c52a9115e113dc8
Chris Udemans <chris.udemans@gmail.com>
2023-03-02 17:02:28 +0200

    build: update naive bayes to accept multidimensional data

--- src/naive_bayes/NaiveBayes.js
+++ src/naive_bayes/NaiveBayes.js
@@ -1,10 +1,11 @@
-import { getMean, getVariance } from "summarystats";
+import mean from "../utils/mean.js";
+import variance from "../utils/variance.js";
 
 export default class NaiveBayes {
-  constructor(X, y) {
-    this.nSamples = X.length;
-    this.nFeatures = X[0].length;
-    this.nClasses = [...new Set(y)];
+  constructor() {
+    this.nSamples = null;
+    this.nFeatures = null;
+    this.nClasses = null;
     this.eps = 1e-6;
     this.classesMean = {};
     this.classesVar = {};
@@ -12,25 +13,27 @@ export default class NaiveBayes {
   }
 
   fit(X, y) {
-    for (let i = 0; i < this.nClasses.length; i++) {
+    this.nSamples = X.length;
+    this.nFeatures = X[0].length;
+    this.nClasses = [...new Set(y)];
+
+    this.nClasses.forEach((i) => {
       const mask = y
         .map((d, j) => {
           if (d == i) {
             return j;
           }
         })
-        .filter((el) => el);
+        .filter((el) => el != undefined);
 
-      const X_c = X.map((d, i) => {
-        if (mask.contains(i)) {
-          return d;
-        }
-      });
+      const X_c = mask.map((d) => X[d]);
 
-      this.classesMean[i] = getMean(X_c);
-      this.classesVar[i] = getVariance(X_c);
+      this.classesMean[i] = this.nFeatures < 2 ? mean(X_c, 0) : mean(X_c, 1);
+      console.log(this.classesMean);
+      this.classesVar[i] =
+        this.nFeatures < 2 ? variance(X_c, 0) : variance(X_c, 1);
       this.classesPrior[i] = X_c.length / this.nSamples;
-    }
+    });
   }
   predict(X) {
     return;
